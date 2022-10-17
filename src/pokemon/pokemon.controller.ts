2a592e49b16a2026b960b9415c6cfae6afb8fc64
Yerai Espinar Moreno <yespinar.moreno@atsistemas.com>
2022-10-17 10:54:25 +0200

    test

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
@@ -7,7 +15,7 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 export class PokemonController {
   constructor(private readonly pokemonService: PokemonService) {}
 
-  @Post()
+  /* @Post()
   create(@Body() createPokemonDto: CreatePokemonDto) {
     return this.pokemonService.create(createPokemonDto);
   }
@@ -15,14 +23,14 @@ export class PokemonController {
   @Get()
   findAll() {
     return this.pokemonService.findAll();
-  }
+  } */
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.pokemonService.findOne(+id);
   }
 
-  @Patch(':id')
+  /* @Patch(':id')
   update(@Param('id') id: string, @Body() updatePokemonDto: UpdatePokemonDto) {
     return this.pokemonService.update(+id, updatePokemonDto);
   }
@@ -30,5 +38,5 @@ export class PokemonController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.pokemonService.remove(+id);
-  }
+  } */
 }
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -4,23 +4,23 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+  /* create(createPokemonDto: CreatePokemonDto) {
     return 'This action adds a new pokemon';
   }
 
   findAll() {
     return `This action returns all pokemon`;
-  }
+  } */
 
   findOne(id: number) {
     return `This action returns a #${id} pokemon`;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
+  /* update(id: number, updatePokemonDto: UpdatePokemonDto) {
     return `This action updates a #${id} pokemon`;
   }
 
   remove(id: number) {
     return `This action removes a #${id} pokemon`;
-  }
+  } */
 }
